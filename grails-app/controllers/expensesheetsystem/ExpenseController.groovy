79528f7b7c6dfc35fb6b81db984267010cefea5f
William Hankey <williamhankey1@gmail.com>
2021-06-09 04:31:38 +0200

    updated

--- grails-app/controllers/expensesheetsystem/ExpenseController.groovy
+++ grails-app/controllers/expensesheetsystem/ExpenseController.groovy
@@ -1,11 +1,21 @@
 package expensesheetsystem
 
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 import grails.validation.ValidationException
 import static org.springframework.http.HttpStatus.*
 
-class ExpenseController {
+class ExpenseController implements GrailsConfigurationAware{
 
     ExpenseService expenseService
+
+    UserService userService
+
+    CurrencyConverterService currencyConverterService
+
+    String csvMimeType
+
+    String encoding
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
@@ -85,6 +95,50 @@ class ExpenseController {
             }
             '*'{ render status: NO_CONTENT }
         }
+    }
+
+    def statement(Long id) {
+        def user = getUserStatement(id)
+
+        respond user
+    }
+
+    def downloadCSV(Long id) {
+        final String filename = 'statement.csv'
+        def user = getUserStatement(id)
+        def lines = user.expenses.collect { [it.dateCreated, it.description, it.amount, it.convertedAmount, it.runningBalance].join(';') } as List<String>
+
+        def outs = response.outputStream
+        response.status = OK.value()
+        response.contentType = "${csvMimeType};charset=${encoding}";
+        response.setHeader "Content-disposition", "attachment; filename=${filename}"
+
+        lines.each { String line ->
+            outs << "${line}\n"
+        }
+
+        outs.flush()
+        outs.close()
+    }
+
+    protected getUserStatement(Long id){
+        def user = userService.get(id)
+        user.expenses = user.expenses.sort {it.dateCreated}
+        def runningbalance = user.initialBankBalance
+        for(expense in user.expenses) {
+            expense.runningBalance = runningbalance - expense.amount
+            runningbalance = expense.runningBalance
+            expense.convertedAmount = currencyConverterService.convertRandToUSD(expense.amount)
+        }
+
+        return user
+    }
+
+    @Override
+    void setConfiguration(Config co) {
+        csvMimeType = co.getProperty('grails.mime.types.csv', String, 'text/csv')
+        encoding = co.getProperty('grails.converters.encoding', String, 'UTF-8')
+
     }
 
     protected void notFound() {
