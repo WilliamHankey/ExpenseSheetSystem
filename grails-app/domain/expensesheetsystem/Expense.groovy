8ac5aca71ad217a3f17a600e6a9a6dbf870a4c1e
William Hankey <williamhankey1@gmail.com>
2021-06-09 04:23:28 +0200

    Update domain classes User and Expense

--- grails-app/domain/expensesheetsystem/Expense.groovy
+++ grails-app/domain/expensesheetsystem/Expense.groovy
@@ -1,7 +1,26 @@
 package expensesheetsystem
 
+import java.sql.Timestamp
+
 class Expense {
 
+    Date dateCreated
+    Double amount 
+    String description
+    Double runningBalance = 0.0
+    Double convertedAmount 
+
+    static transients = ['runningBalance', 'convertedAmount']
+
+    static belongsTo = [user: User]
+
     static constraints = {
+        description blank: false
+        amount blank: false
+        
+    }
+
+    String toString() {
+        return description+" - "+amount
     }
 }
--- grails-app/domain/expensesheetsystem/User.groovy
+++ grails-app/domain/expensesheetsystem/User.groovy
@@ -2,6 +2,18 @@ package expensesheetsystem
 
 class User {
 
+    String name
+    Double initialBankBalance
+
+    static hasMany = [expenses: Expense]
+
     static constraints = {
+        name blank: false, unique: true
+        initialBankBalance blank: false
+    }
+
+    String toString() {
+        return name
     }
 }
+
