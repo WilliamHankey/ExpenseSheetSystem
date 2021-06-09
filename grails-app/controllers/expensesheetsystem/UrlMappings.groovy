a9dd04a31adbd30761e84def108af438dd720082
William Hankey <williamhankey1@gmail.com>
2021-06-09 04:32:06 +0200

    Update UrlMappings.groovy

--- grails-app/controllers/expensesheetsystem/UrlMappings.groovy
+++ grails-app/controllers/expensesheetsystem/UrlMappings.groovy
@@ -9,7 +9,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/"(controller: "user", action: "create")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
